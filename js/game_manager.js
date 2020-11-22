5b1de71fc3949cd5999e6021c9eba88a8f4475f8
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-11-21 21:33:43 -0600

    Spawn higher tiles

--- js/game_manager.js
+++ js/game_manager.js
@@ -68,7 +68,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 1099511627776 : 2199023255552;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -166,8 +166,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 9007199254740992 tile
+          if (merged.value === 9007199254740992) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
